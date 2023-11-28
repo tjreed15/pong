b28fdf5e2f85663d3c926caa3000c385c4c93af9
TJ Reed <tjreed15@gmail.com>
2023-11-27 21:57:22 -0800

    Added minor pause menu

--- godot/controllers/GameController.gd
+++ godot/controllers/GameController.gd
@@ -1,6 +1,8 @@
 class_name GameController extends Node
 
+@onready var pauseMenu: Control = %PauseMenu
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		get_tree().paused = not get_tree().paused
+		self.pauseMenu.visible = not self.pauseMenu.visible
